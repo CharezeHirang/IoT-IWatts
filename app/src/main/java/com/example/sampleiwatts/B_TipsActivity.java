d08425f29637e60c7fde3c1832be71a71fc9c62c
Chareze Hirang <charezehirang@gmail.com>
2025-08-17 18:08:36 +0800

    Sundays Commit

--- app/src/main/java/com/example/sampleiwatts/B_TipsActivity.java
+++ app/src/main/java/com/example/sampleiwatts/B_TipsActivity.java
@@ -1,6 +1,8 @@
 package com.example.sampleiwatts;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +17,12 @@ public class B_TipsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_btips);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        ImageView icBack = findViewById(R.id.ic_back);
+        icBack.setOnClickListener(v -> {
+            Intent intent = new Intent(B_TipsActivity.this, TipsActivity.class);
+            startActivity(intent);
+            finish();
         });
     }
 }
--- app/src/main/java/com/example/sampleiwatts/E_TipsActivity.java
+++ app/src/main/java/com/example/sampleiwatts/E_TipsActivity.java
@@ -1,6 +1,8 @@
 package com.example.sampleiwatts;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +17,11 @@ public class E_TipsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_etips);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        ImageView icBack = findViewById(R.id.ic_back);
+        icBack.setOnClickListener(v -> {
+            Intent intent = new Intent(E_TipsActivity.this, TipsActivity.class);
+            startActivity(intent);
+            finish();
         });
     }
 }
