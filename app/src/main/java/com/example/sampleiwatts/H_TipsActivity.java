4ad4f18478da92b883a80e30dc7d5010e3145bd0
Chareze Hirang <charezehirang@gmail.com>
2025-08-25 11:53:58 +0800

    commit

---
+++ app/src/main/java/com/example/sampleiwatts/H_TipsActivity.java
@@ -0,0 +1,29 @@
+package com.example.sampleiwatts;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class H_TipsActivity extends AppCompatActivity {
+
+    ImageView icBack;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_htips);
+
+        icBack = findViewById(R.id.ic_back);
+        icBack.setOnClickListener(v -> {
+            Intent intent = new Intent(H_TipsActivity.this, TipsActivity.class);
+            startActivity(intent);
+            finish();
+        });
+    }
+}
